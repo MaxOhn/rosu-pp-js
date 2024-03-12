fc11335b3e9189d920706fbe3d3203517dc55194
MaxOhn <ohn.m@hotmail.de>
2024-03-12 23:53:57 +0100

    add optional wee_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,10 @@ mod performance;
 mod score_state;
 mod strains;
 
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
